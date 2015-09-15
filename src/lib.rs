2c9f7b85d546c83921c87c25cd7df90333a18d2f
dremonkey <a.deutmeyer@gmail.com>
2015-09-15 15:34:42 -0700

    new: move constants to lodestone-core, use build in to_degrees and to_radians

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,12 @@
 /// Calculates the distance between two points on the surface of the earth using 
 /// the [Haversine formula](http://en.wikipedia.org/wiki/Haversine_formula).
 
-// Standard lib packages
-use std::f64::consts::{PI};
-
 // Third party packages
 extern crate lodestone_point;
+extern crate lodestone_core;
 
 use lodestone_point::FeaturePoint;
-
-// Radius of the Earth
-// http://www.wolframalpha.com/input/?i=average+radius+of+Earth
-const RADIUS_KM: f64 = 6367.4447;
-const RADIUS_MI: f64 = 3956.5467;
-const RADIUS_DEGREES: f64 = 57.2957795;
+use lodestone_core::{utils, wgs84};
 
 pub extern fn distance(
     from_point: &FeaturePoint,
@@ -27,26 +20,25 @@ pub extern fn distance(
   let coord1 = from_point.coordinates();
   let coord2 = to_point.coordinates();
 
-  let lat1 = to_rad(coord1[1]);
-  let lat2 = to_rad(coord2[1]);
-  let dlat = to_rad(coord2[1] - coord1[1]);
-  let dlng = to_rad(coord2[0] - coord1[0]);
+  let lat1 = coord1[1].to_radians();
+  let lat2 = coord2[1].to_radians();
+  let dlat = (coord2[1] - coord1[1]).to_radians();
+  let dlng = (coord2[0] - coord1[0]).to_radians();
 
   let a = (dlat/2.0).sin() * (dlat/2.0).sin() +
           lat1.cos() * lat2.cos() * (dlng/2.0).sin() * (dlng/2.0).sin();
   let c = 2.0 * a.sqrt().atan2((1.0-a).sqrt());
 
   match units {
-    "degrees" => RADIUS_DEGREES * c,
-    "kilometers" => RADIUS_KM * c,
-    "miles" => RADIUS_MI * c,
+    "degrees" => c.to_degrees(),
+    "kilometers" => c * wgs84::RADIUS / 1000.0,
+    "miles" => {
+      let radius_mi = utils::km_to_mi(wgs84::RADIUS / 1000.0);
+      c * radius_mi
+    },
     "radians" => c,
-    _ => RADIUS_KM * c,
+    _ => c * wgs84::RADIUS / 1000.0,
   }
-}
-
-fn to_rad(degree: f64) -> f64 {
-  degree * PI / 180.0
 }
 
 #[cfg(test)]
@@ -65,8 +57,8 @@ mod tests {
     let d_km = distance(&sf_point, &ny_point, "kilometers");
     let d_mi = distance(&sf_point, &ny_point, "miles");
 
-    assert_eq!(d_km, 4126.249115079681);
-    assert_eq!(d_mi, 2563.9323290308953);
+    assert_eq!(d_km, 4133.177968880825);
+    assert_eq!(d_mi, 2568.236927701447);
   }
 
   #[test]
@@ -79,7 +71,7 @@ mod tests {
     let d_km = distance(&sf_point, &la_point, "kilometers");
     let d_mi = distance(&sf_point, &la_point, "miles");
 
-    assert_eq!(d_km, 559.1277487787996);
-    assert_eq!(d_mi, 347.4258754550611);
+    assert_eq!(d_km, 560.0666437217378);
+    assert_eq!(d_mi, 348.00917047601996);
   }
 }
